42c74a69bb5234f705e301d912765b0790ceee0e
menna <mennamamdouh1223334455@ail.@@m@@>
2023-03-17 14:30:57 -0700

    new

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ * Return: Always 0 (Success)
+ */
 int main(void)
+
 {
-char ch = 'a';
-while (ch <= 'z')
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
 {
-putchar(ch);
-ch++;
+putchar(alp[i]);
 }
 putchar('\n');
 return (0);
