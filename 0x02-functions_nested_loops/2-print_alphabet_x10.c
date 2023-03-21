b1be029ffa7b17e04291daebc4ee010d0019fb23
menna <mennamamdouh1223334455@ail.@@m@@>
2023-03-21 16:26:23 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print 10 lowercase char
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar('a' + i);
+}
+_putchar('\n');
+}
