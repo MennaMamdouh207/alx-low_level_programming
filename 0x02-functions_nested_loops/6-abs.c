322df8fcd54bd652c50f0f5f38a6192acdfd2276
menna <mennamamdouh1223334455@ail.@@m@@>
2023-03-21 18:45:04 -0700

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abc - Computes the bslute valeu
+ *@c: the number will be computed
+ *Return: Abslute of valeu
+ *
+ */
+int _abc(int c)
+{
+if (c < 0)
+{
+int abc_val;
+abc_val = c * -1;
+return (abc_val);
+}
+return (0);
+}
